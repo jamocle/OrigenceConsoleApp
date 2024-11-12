e0fd257d182d4da33530e024d9c15decf362b5b9
James Theimer <jimmy@thetheimers.com>
2024-11-12 16:47:47 -0500

    Initial StringRemoval addition for request #123

--- OrigenceConsoleApp/Program.cs
+++ OrigenceConsoleApp/Program.cs
@@ -4,6 +4,37 @@
 using OrigenceConsoleApp;
 using OrigenceConsoleApp.Contracts;
 
+//Remove Duplicates
+
+/* Requirements:
+* Remove all duplicate characters in a string
+* If string is null return null;
+* if string is empty return empty
+*/
+
+//Test case 1
+//null string  Should return "IsNull"
+string? x = null;
+Console.WriteLine(x.RemoveDuplicates() ?? "IsNull");
+
+//Test case 2
+//Empty string  Should return ''
+x = string.Empty;
+Console.WriteLine($"'{x.RemoveDuplicates()}'");
+
+//Test case 3
+//xyzzy string  Should return 'xyz'
+x = "xyzzy";
+Console.WriteLine($"'{x.RemoveDuplicates()}'");
+
+//Test case 4
+//xbcdr string  Should return 'xbcdr'
+x = "xbcdr";
+Console.WriteLine($"'{x.RemoveDuplicates()}'");
+
+
+return;
+//Create a stack
 IStack<string> stack = new MyStack<string>();
 
 stack.Push("1");
